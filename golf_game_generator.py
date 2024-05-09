4f6321c7ef1a3adf6463d5334ff113eeef7d6484
Mark Custard <markcustard@Marks-MacBook-Pro.local>
2024-05-09 15:39:11 -0700

    Refactored code to be less DRY and updated unit tests accordingly.

--- golf_game_generator.py
+++ golf_game_generator.py
@@ -7,25 +7,23 @@ class GolfGame:
     def generate_course_info(self):
         course = random.choice(self.courses)
         crowd = self.generate_crowd()
+        time_of_day = self.generate_time_of_day()
         tee = self.generate_tee_location(course)
         pin = self.generate_pin_location()
         wind = self.generate_wind()
-        green_firmness = self.generate_green_firmness()
-        green_speed = self.generate_green_speed()
-        fringe_firmness = self.generate_fringe_firmness()
-        fringe_firmness_speed = self.generate_fringe_speed()
-        fairway_firmness = self.generate_fairway_firmness()
-        fairway_speed = self.generate_fairway_speed()
-        first_cut_firmness = self.generate_first_cut_firmness()
-        first_cut_length = self.generate_first_cut_length()
-        second_cut_firmness = self.generate_second_cut_firmness()
-        second_cut_length = self.generate_second_cut_length()
+        green_firmness, green_speed = self.generate_condition()
+        fringe_firmness, fringe_speed = self.generate_condition()
+        fairway_firmness, fairway_speed = self.generate_condition()
+        first_cut_firmness, first_cut_length = self.generate_condition(True)
+        second_cut_firmness, second_cut_length = self.generate_condition(True)
         
-        return course, crowd, tee, pin, wind, green_firmness, green_speed, fringe_firmness, fringe_firmness_speed, fairway_firmness, fairway_speed, first_cut_firmness, first_cut_length, second_cut_firmness, second_cut_length
+        return course, crowd, tee, pin, wind, green_firmness, green_speed, fringe_firmness, fringe_speed, fairway_firmness, fairway_speed, first_cut_firmness, first_cut_length, second_cut_firmness, second_cut_length, time_of_day
     
     def generate_crowd(self):
-        return random.choice(['On', 'Off']
-        )
+        return random.choice(['On', 'Off'])
+    
+    def generate_time_of_day(self):
+        return random.choice(["Morning", "Afternoon", "Evening"])
 
     def generate_tee_location(self, course):
         if course == 'Augusta':
@@ -45,36 +43,44 @@ class GolfGame:
         directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
         return random.choice(directions), random.choice(speed_ranges)
     
-    def generate_green_firmness(self):
-        return random.choice(["Soft", "Average", "Firm", "Tournament"])
-    
-    def generate_green_speed(self):
-        return random.choice(["Slow", "Average", "Fast", "Tournament"])
+    def generate_condition(self, is_cut=False):
+        if is_cut:
+            firmness_levels = ["Soft", "Firm", "Tournament"]
+        else:
+            firmness_levels = ["Soft", "Average", "Firm", "Tournament"]
+        speed_levels = ["Slow", "Average", "Fast", "Tournament"]
+        
+        firmness = random.choice(firmness_levels)
+        
+        if is_cut:
+            length_levels = ["Short", "Medium", "Long"]
+            length = random.choice(length_levels)
+            return firmness, length
+        else:
+            speed = random.choice(speed_levels)
+            return firmness, speed
 
-    def generate_fringe_firmness(self):
-        return random.choice(["Soft", "Average", "Firm", "Tournament"])
-    
-    def generate_fringe_speed(self):
-        return random.choice(["Slow", "Average", "Fast", "Tournament"])
-    
-    def generate_fairway_firmness(self):
-        return random.choice(["Soft", "Average", "Firm", "Tournament"])
-    
-    def generate_fairway_speed(self):
-        return random.choice(["Slow", "Average", "Fast", "Tournament"])
-    
-    def generate_first_cut_firmness(self):
-        return random.choice(["Soft", "Firm", "Tournament"])
-    
-    def generate_first_cut_length(self):
-        return random.choice(["Short", "Medium", "Long"])
-    
-    def generate_second_cut_firmness(self):
-        return random.choice(["Soft", "Firm", "Tournament"])
-    
-    def generate_second_cut_length(self):
-        return random.choice(["Short", "Medium", "Long"])
-    
+def print_course_info(course_info):
+    print("Generated Golf Game Information:")
+    print("Course:".ljust(20), course_info[0])
+    print("Crowd:".ljust(20), course_info[1])
+    print("Time of Day:".ljust(20), course_info[15])
+    print("Tee:".ljust(20), course_info[2])
+    print("Pin:".ljust(20), course_info[3])
+    print("Wind Speed:".ljust(20), course_info[4][1])
+    print("Wind Direction:".ljust(20), course_info[4][0])
+    print("Green Firmness:".ljust(20), course_info[5])
+    print("Green Speed:".ljust(20), course_info[6])
+    print("Fringe Firmness:".ljust(20), course_info[7])
+    print("Fringe Speed:".ljust(20), course_info[8])
+    print("Fairway Firmness:".ljust(20), course_info[9])
+    print("Fairway Speed:".ljust(20), course_info[10])
+    print("First Cut Firmness:".ljust(20), course_info[11])
+    print("First Cut Length:".ljust(20), course_info[12])
+    print("Second Cut Firmness:".ljust(20), course_info[13])
+    print("Second Cut Length:".ljust(20), course_info[14])
+
+
 def main():
     courses = [
         'Augusta', 'Bandon Dunes', 'Banff Springs', 'Bay Hill', 'Chambers Bay',
@@ -90,23 +96,8 @@ def main():
     game = GolfGame(courses)
     course_info = game.generate_course_info()
 
-    print("Generated Golf Game Information:")
-    print("Course:", course_info[0])
-    print("Crowd:", course_info[1])
-    print("Tee:", course_info[2])
-    print("Pin:", course_info[3])
-    print("Wind Direction:", course_info[4][0])
-    print("Wind Speed:", course_info[4][1])
-    print("Green Firmness:", course_info[5])
-    print("Green Speed:", course_info[6])
-    print("Fringe Firmness:", course_info[7])
-    print("Fringe Speed:", course_info[8])
-    print("Fairway Firmness:", course_info[9])
-    print("Fairway Speed:", course_info[10])
-    print("First Cut Firmness:", course_info[11])
-    print("First Cut Length:", course_info[12])
-    print("Second Cut Firmness:", course_info[13])
-    print("Second Cut Length:", course_info[14])
+    print_course_info(course_info)
+
 
 if __name__ == "__main__":
     main()
